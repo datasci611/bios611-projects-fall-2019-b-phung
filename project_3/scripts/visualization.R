bae3b9c5a4778d31ad7b1d2cab792619efe1bbe8
Brian <bphung@live.unc.edu>
2019-12-02 18:47:15 -0500

    Update visualization.R

--- project_3/scripts/visualization.R
+++ project_3/scripts/visualization.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
+# import data wrangled from python
 zip2 = read_csv('data/zip.csv') 
 zip2$ZC = as.character(zip2$ZC)
 
@@ -20,7 +21,7 @@ main = zip2 %>%
 # However, there is an argument for not subsetting. I might explore how this affects the results.
 
 
-# There are 290 unique ZIP codes, so to limit how many to plot,
+# There are 278 unique ZIP codes, so to limit how many to plot,
 # we can choose those that had at least some threshold (e.g. 15 here) clients.
 length(unique(main$ZC))
 min_visits = 15
@@ -31,9 +32,9 @@ plot_violin = main2 %>%
   ggplot(aes(factor(ZC), HPI)) +
   geom_violin(scale = "count") +
   geom_text(aes(x=factor(ZC), y=max(HPI)+25, label=ZCcounts)) +
-  xlab('Zip Code') +
+  xlab('ZIP Code') +
   ylab('Housing Price Index') +
-  ggtitle('Zip Codes that had >=15 visitors and violin plots of their HPI')
+  ggtitle('ZIP Codes that had >=15 visitors and violin plots of their HPI')
 plot_violin
 ggsave("results/plot_violin.png", width = 16/2, height = 9/2)
 # Note that UMD is located in 27701.
@@ -48,14 +49,14 @@ main3 = main2 %>% group_by(ZC, year) %>% mutate(ZCyearcounts=n())
 plot_hpi = main3 %>% ggplot(aes(year, HPI, group=ZC)) +
   geom_line(aes(color=ZC)) +
   ylab('Housing Price Index') +
-  ggtitle('HPI over years grouped by Zip Code')
+  ggtitle('HPI over years grouped by ZIP Code')
 plot_hpi
 ggsave("results/plot_hpi.png", width = 16/4, height = 9/2)
 
 plot_visits = main3 %>% ggplot(aes(year, ZCyearcounts, group=ZC)) +
   geom_line(aes(color=ZC)) +
   ylab('visits') +
-  ggtitle('Visits over years grouped by Zip Code')
+  ggtitle('Visits over years grouped by ZIP Code')
 plot_visits
 ggsave("results/plot_visits.png", width = 16/4, height = 9/2)
 
@@ -69,9 +70,8 @@ library(rgeos)
 library(maptools)
 library(ggalt)
 library(ggthemes)
-library(ggrepel)
-library(RColorBrewer)
 
+# generate a list of the ZIP codes we are interested in
 raleigh.durham <- c(seq(from=27701, to=27722), 
                     seq(from=27601, to = 27699))
 
@@ -81,20 +81,20 @@ mypoly <- subset(mydata, ZCTA5CE10 %in% raleigh.durham)
 mymap <- fortify(mypoly)
 plot(mypoly)
 
+
 latlong = read_csv("https://gist.githubusercontent.com/erichurst/7882666/raw/5bdc46db47d9515269ab12ed6fb2850377fd869e/US%2520Zip%2520Codes%2520from%25202013%2520Government%2520Data")
 main4 = main2 %>% select(ZC, ZCcounts) %>% unique() %>%
   left_join(latlong, by = c("ZC" = "ZIP"))
 mypoly.df <- as(mypoly, "data.frame") %>%
   left_join(main4, by = c("ZCTA5CE10" = "ZC"))
 
-
+# plot each polygon along with ZIP code and number of visits
 plot_map = ggplot() +
   geom_cartogram(data = mymap, aes(x = long, y = lat, map_id = id), map = mymap, fill="white", color="black", size=.05) +
   geom_text(data = mypoly.df, aes(label = ZCTA5CE10, x = LNG, y = LAT), size = 1.5, nudge_y = .005) +
   geom_text(data = mypoly.df, aes(label = ZCcounts, x = LNG, y = LAT), size = 1.5, nudge_y = -.005, color = "red") +
   theme_map() +
   coord_map() +
-  ggtitle("Map of ZIP Codes Durham and Raleigh with Visits>=15 Labelled")
+  ggtitle("Map of ZIP Codes Durham and Raleigh with >=15 Visitors Labeled")
 plot_map
-ggsave("results/plot_map.png", width = 16/2, height = 9/2)            
-
+ggsave("results/plot_map.png", width = 16/2, height = 9/2)
