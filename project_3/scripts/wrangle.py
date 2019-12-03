526788c62b10528a3e58fdc635ed3e25b01a73fa
Brian <bphung@live.unc.edu>
2019-12-02 20:11:32 -0500

    Update wrangle.py

--- project_3/scripts/wrangle.py
+++ project_3/scripts/wrangle.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 zip = pd.read_csv("https://raw.githubusercontent.com/biodatascience/datasci611/gh-pages/data/project2_2019/EE_UDES_191102.tsv", sep='\t',
 	usecols=['Client ID', 'EE UID', 'Zip Code (of Last Permanent Address, if known)(1932)'],
@@ -7,17 +8,23 @@ zip = pd.read_csv("https://raw.githubusercontent.com/biodatascience/datasci611/g
 zip['ZC'] = zip[zip.columns[2]].str.slice(0,5)
 zip = zip[zip.ZC.str.contains(r'\d{5}', na=False)]
 del zip['Zip Code (of Last Permanent Address, if known)(1932)']
-zip.to_csv("../data/zip.csv", index=False, header=True)
+zip_path = '../data/zip.csv'
+zip_path = os.path.join(os.path.dirname(__file__), zip_path)
+zip.to_csv(zip_path, index=False, header=True)
 
 
 year = pd.read_csv("https://raw.githubusercontent.com/biodatascience/datasci611/gh-pages/data/project2_2019/ENTRY_EXIT_191102.tsv", sep='\t',
 	usecols=['EE UID', 'Entry Date'])
 year['year'] = year[year.columns[1]].str.slice(-4)
 del year['Entry Date']
-year.to_csv("../data/year.csv", index=False, header=True)
+year_path = '../data/year.csv'
+year_path = os.path.join(os.path.dirname(__file__), year_path)
+year.to_csv(year_path, index=False, header=True)
 
 
 hpi = pd.read_csv("https://raw.githubusercontent.com/datasci611/bios611-projects-fall-2019-b-phung/master/project_3/data/hpi.csv",
 	converters={'Five-Digit ZIP Code': lambda x: str(x)},
 	skiprows = 6, usecols=['Five-Digit ZIP Code', 'Year', 'HPI'])
-hpi.to_csv("../data/hpi_wrangled.csv", index=False, header=True)
+hpi_path = '../data/hpi_wrangled.csv'
+hpi_path = os.path.join(os.path.dirname(__file__), hpi_path)
+hpi.to_csv(hpi_path, index=False, header=True)
